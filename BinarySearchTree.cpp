3fc9be62da89e5d557b4f94a03eb7b6d4f50dcb5
fairus0305 <ruszezzy@gmail.com>
2026-06-12 15:07:28 +0700

    membuat constructor untuk class node

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -2,11 +2,18 @@
 using namespace std;
 
 //membuat class node
-class node {
+class Node {
 
     //memberi akses modifer serta mendeklarasi variabel dan object node
     public :
         string info;
         Node* leftChild;
         Node* rightChild;
+
+        //membuat constructor untuk class node
+        Node(string i, Node* I, Node* r){
+            
+        }
+
+
 }
